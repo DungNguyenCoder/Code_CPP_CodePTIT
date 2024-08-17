04fdbcebd9dfed7ab81c1df169453bbac2f85b80
DungNguyen9999 <dungnickchinh1909@gmail.com>
2024-08-18 00:07:11 +0700

    project

---
+++ CHELLO.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define faster() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define vt vector
+#define db double
+
+const int mx = 1e5;
+const int mod = 1e9+7;
+#define TEST 0
+
+inline void solution()
+{
+    cout << "Hello PTIT.";
+}
+
+int main()
+{
+    faster();
+    int t;
+    if(TEST)
+    {
+        cin >> t;
+        cin.ignore();
+    }
+    else        t = 1;
+    while(t--)
+    {
+        solution();
+    }
+}
