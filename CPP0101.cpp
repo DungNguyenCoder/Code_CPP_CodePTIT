0017ca17b96548f30f763c8485074ae28deccb3a
DungNguyen9999 <dungnickchinh1909@gmail.com>
2024-08-18 00:18:23 +0700

    1

---
+++ CPP0101.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define faster() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define vt vector
+#define db double
+
+const int mx = 1e5;
+const int mod = 1e9+7;
+#define TEST 1
+
+inline void solution()
+{
+    ll n;
+    cin >> n;
+    ll s = (n*(n+1))/2;
+    cout << s << "\n";
+}
+
+int main()
+{
+    faster();
+    int t;
+    if(TEST)
+    {
+        cin >> t;
+        cin.ignore();
+    }
+    else        t = 1;
+    while(t--)
+    {
+        solution();
+    }
+}
