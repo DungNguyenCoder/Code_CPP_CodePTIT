c6151cbcf71d4866a429cea10b7754c4e36597b3
DungNguyenCoder <dungnickchinh1909@gmail.com>
2024-09-04 16:11:05 +0700

    CPP

---
+++ CPP0113.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define faster() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+#define ll long long
+#define fi first
+#define se second
+#define vt vector
+#define db double
+
+const int mx = 1e5;
+const int mod = 1e9+7;
+#define TEST 1
+
+inline void solution()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    if(s[n-2] == '8' && s[n-1] == '6')
+        cout << "1\n";
+    else
+        cout << "0\n";
+}
+
+int main()
+{
+    faster();
+    int t;
+    if(TEST)
+    {
+        cin >> t;
+        cin.ignore();
+    }
+    else        t = 1;
+    while(t--)
+    {
+        solution();
+    }
+}
